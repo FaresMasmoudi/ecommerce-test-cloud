dd31c6a5868513f75d3e1591b6bca6b6de6bcd93
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-05-04 13:29:43 +0100

    port no update

--- components/connect.php
+++ components/connect.php
@@ -4,9 +4,10 @@ $servername = "192.168.211.131";
 $username = "root";
 $password = "password";
 $dbname = "shop_db"; // replace with your database name
+$port = 3306;
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
